ce31759380d78b6d82420054ea86a7f7440844fc
Arya Namiranian <arya1namiranian@gmail.com>
2024-11-22 20:27:41 -0800

    initial commit

---
+++ game.js
@@ -0,0 +1,113 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const heroImg = new Image();
+heroImg.src = "./hero.svg";
+
+let isMouseDown = false;
+let canMove = false;
+
+class Hero {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 42;
+    this.height = 45;
+    this.speed = 3;
+  }
+
+  draw() {
+    ctx.drawImage(heroImg, this.x, this.y);
+  }
+
+  move() {
+    if (canMove) {
+      this.x += this.speed;
+    }
+  }
+}
+
+class Stick {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = 0;
+    this.rotationSpeed = 0.05;
+  }
+
+  grow() {
+    this.height -= 2;
+  }
+
+  rotate() {
+    if (this.angle < Math.PI / 2) {
+      this.angle += this.rotationSpeed;
+    } else {
+      this.angle = Math.PI / 2;
+    }
+  }
+
+  draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+
+    ctx.fillStyle = "#1e1e1e";
+    ctx.fillRect(0, 0, this.width, this.height);
+
+    ctx.restore();
+  }
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+});
+canvas.addEventListener("mouseup", (e) => {
+  isMouseDown = false;
+});
+
+const player = new Hero(10, 350);
+const stick = new Stick(
+  player.x + player.width,
+  player.y + player.height,
+  5,
+  0
+);
+
+function resetStick() {
+  stick.x = player.x + player.width;
+  stick.y = player.y + player.height;
+  stick.width = 5;
+  stick.height = 0;
+  stick.angle = 0;
+}
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  player.move();
+  stick.draw();
+
+  if (isMouseDown) {
+    stick.grow();
+  }
+
+  if (!isMouseDown && stick.height < 0) {
+    stick.rotate();
+    if (stick.angle === Math.PI / 2) {
+      canMove = true;
+    }
+  }
+
+  if (player.x >= stick.height * -1 + stick.x - 10) {
+    canMove = false;
+    resetStick();
+  }
+
+  requestAnimationFrame(gameLoop);
+}
+
+heroImg.onload = () => {
+  gameLoop();
+};
